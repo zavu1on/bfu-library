df55b42d061aa20409232c08836c36ceb27f7c73
Mikhail <joinmma@gmail.com>
2022-07-14 12:38:21 +0200

    add test for useFormater

--- src/hooks/useFormater.ts
+++ src/hooks/useFormater.ts
@@ -1,21 +1,18 @@
 export const useFormater = () => {
   return function (date: Date | string): string {
     if (typeof date === 'string') {
-      const m = new Date(date).getMonth() + 1
-      const d = new Date(date).getDate()
-
-      return `${new Date(date).getFullYear()}-${m >= 10 ? m : '0' + m}-${
-        d >= 10 ? d : '0' + d
-      }`
-    } else if (date instanceof Date) {
-      const m = date.getMonth() + 1
-      const d = date.getDate()
-
-      return `${date.getFullYear()}-${m >= 10 ? m : '0' + m}-${
-        d >= 10 ? d : '0' + d
-      }`
+      return date
     }
 
-    return 'error'
+    let m = date.getMonth()
+    let year = date.getFullYear()
+    const d = date.getDate()
+
+    if (m === 0) {
+      m = 12
+      year -= 1
+    }
+
+    return `${year}-${m >= 10 ? m : '0' + m}-${d >= 10 ? d : '0' + d}`
   }
 }
